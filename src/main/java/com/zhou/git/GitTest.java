7059b9037a174cad9db1b268a6cd3a6c8b6dcad5
zhouhaorong <1317694761@qq.com>
2022-08-11 06:43:13 +0800

    push commit

--- src/main/java/com/zhou/git/GitTest.java
+++ src/main/java/com/zhou/git/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello second123");
         System.out.println("hello second3");
+        System.out.println("hello second3");
+        System.out.println("push test");
     }
 }
