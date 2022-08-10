f9c0f801000e7cb6e07e53f329550cf357ef846c
sso-lad <56908507+sso-lad@users.noreply.github.com>
2022-08-11 06:50:25 +0800

    Update GitTest.java
    hahaha

--- src/main/java/com/zhou/git/GitTest.java
+++ src/main/java/com/zhou/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello second3");
         System.out.println("push test");
         System.out.println("hello test");
+        System.out.println("hello poll");
     }
 }
